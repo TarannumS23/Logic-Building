a356dc3155e2be348eed687b2cc6588ff402a6a7
TarannumS23 <tarannumshaikh01786@gmail.com>
2026-08-06 23:43:48 +0530

    Replace repeated statements with for loop iteration

---
+++ Programming-on-Numbers/Program022_Display_Message_Using_Iteration.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+    int iCnt = 0;
+
+    // Iteration
+    
+    for(iCnt = 1; iCnt <= 4; iCnt++)
+    {
+        printf("Programming Practice..\n");
+    }
+    return 0;
+}
