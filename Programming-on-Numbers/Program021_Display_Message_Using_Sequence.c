a75cbaee658131490effa7fe8e45d5fdad7e4898
TarannumS23 <tarannumshaikh01786@gmail.com>
2026-08-06 23:42:03 +0530

    Add message display using sequential statements

---
+++ Programming-on-Numbers/Program021_Display_Message_Using_Sequence.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+    // Sequence
+    
+    printf("Programming Practice..\n");
+    printf("Programming Practice..\n");
+    printf("Programming Practice..\n");
+    printf("Programming Practice..\n");
+
+    return 0;
+}
